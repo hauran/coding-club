7f17b4f13bd27e5f36e0395d6c4994778a25979c
Richard Mai <richardmai@gmail.com>
2021-02-07 22:39:13 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+
+const MIN = 2
+const MAX = 12
+
+const randomNumber = () => {
+  const min = Math.ceil(MIN)
+  const max = Math.floor(MAX)
+  return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
+}
+
+const generateMathProblem = () => {
+  const num1 = randomNumber()
+  const num2 = randomNumber()
+  const answer = num1 * num2
+  return `${num1} x ${num2} = ${answer}`
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Multiplication Problems</h1>
+        <h2>{generateMathProblem()}</h2>
+        <h2>{generateMathProblem()}</h2>
+        <h2>{generateMathProblem()}</h2>
+        <h2>{generateMathProblem()}</h2>
+        <h2>{generateMathProblem()}</h2>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
